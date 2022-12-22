f9cec195faa9424fccfb29673402bd66f34a9833
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2022-12-22 10:22:35 +0700

    Update AuthController.java

--- src/main/java/com/sop/chapter10/authservice/controller/AuthController.java
+++ src/main/java/com/sop/chapter10/authservice/controller/AuthController.java
@@ -20,11 +20,7 @@ public class AuthController {
     public AuthController(final AuthService authService){
         this.authService = authService;
     }
-// ใช้ได้เหมือนกัน
-//    @PostMapping(value = "/login")
-//    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest){
-//        return  ResponseEntity.ok(authService.logIn(authRequest));
-//    }
+
     @PostMapping(value = "/register")
     public ResponseEntity<?> register(@RequestBody User user){
         return ResponseEntity.ok(authService.register(user));
@@ -40,15 +36,5 @@ public ResponseEntity<?> logIn(@RequestBody AuthRequest authRequest) {
         return authService.getAllData();
     }
 
-//    @GetMapping(value = "/me")
-//    public ResponseEntity<?> me(@RequestHeader(value = "Authorization") String token, @RequestHeader(value = "id") String id, @RequestHeader(value = "email") String email, @RequestHeader(value = "role") String role) {
-//        System.out.println("Authorization : " + token);
-//        return ResponseEntity.ok(authService.me(id, email, role));
-//    }
 
-    @GetMapping(value = "/me")
-    public ResponseEntity<?> me(@RequestHeader(value = "Authorization") String token, @RequestHeader(value = "id") String id, @RequestHeader(value = "email") String email, @RequestHeader(value = "role") String role) {
-        System.out.println("Authorization : " + token);
-        return ResponseEntity.ok(authService.me(id, email, role));
-    }
 }
